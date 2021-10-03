64827bcaddcae3c7ea52ab344875db26feab30bd
dangchihao <dangchihao123@gmail.com>
2021-10-03 16:40:16 +0700

    hiển thị sản phẩm từ trong store

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import { connect } from "react-redux";
 
 class Products extends Component {
   render() {
+    let { product } = this.props;
     return (
       <section className="section">
         <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
         <div className="row">
-          {/* Product */}
-          <Product />
-          <Product />
-          <Product />
+          {/*
+           *    gọi là hiển thị sản phẩm và đối số truyền vào là state trong store chuyển về component là 1 cái props
+           */}
+          {this.showProduct(product)}
         </div>
       </section>
     );
   }
+
+  // Hàm hiển thị sản phẩm nhận vào tham số là 1 mảng các object sản phẩm
+  showProduct = (products) => {
+    var result = null;
+    if (products.length > 0) {
+      result = products.map((product, index) => {
+        return <Product index={index} product={product} key={product.id} />;
+      });
+    }
+    return result;
+  };
 }
 
-export default Products;
+const mapStateToProps = (state) => {
+  return {
+    product: state.products,
+  };
+};
+
+export default connect(mapStateToProps, null)(Products);
